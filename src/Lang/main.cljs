4bcd3784eb636002b86c4cf5e00063644e7e46ae
Lang <Lang@nice-armor>
2022-05-08 16:40:49 +0300

    i launch orbitdb :Bob-Mortimer thumbs are for gripping not ripping

--- src/Lang/main.cljs
+++ src/Lang/main.cljs
@@ -21,11 +21,17 @@
    [Lang.corn]
    [Lang.beans]))
 
+(defonce os (js/require "os"))
 (defonce fs (js/require "fs"))
 (defonce path (js/require "path"))
 (defonce express (js/require "express"))
+(defonce OrbitDB (js/require "orbit-db"))
+(defonce IPFSHttpClient (js/require "ipfs-http-client"))
+(defonce AbortController (.-AbortController (js/require "node-abort-controller")))
 
-(defonce ^:const PORT 3000)
+(set! (.-AbortController js/global) AbortController)
+
+(defonce ^:const PORT 3301)
 (def server (express))
 
 (.use server (.static express "ui"))
@@ -33,13 +39,25 @@
 (.get server "*" (fn [request response]
                    (.sendFile response (.join path js/__dirname  "ui" "index.html"))))
 
-(.listen server 3000
-         (fn []
-           (js/console.log (format "server started on http://localhost:%s" PORT))))
-
 (defn -main []
-  (println ":_ what is this thing?")
-  (println ":Mando i keep it around for luck")
-  (println ":_ you're gonna needed where you're headed")
-  (println "i dont want my next job")
-  (println "Kuiil has spoken"))
+  (go
+    (let [complete| (chan 1)]
+      (.listen server PORT
+               (fn [] (put! complete| true)))
+      (<! complete|)
+      (println ":_ what is this thing?")
+      (println ":Mando i keep it around for luck")
+      (println (format ":_ you're gonna needed where you're headed - http://localhost:%s" PORT))
+      (println "i dont want my next job")
+      (println "Kuiil has spoken")
+
+      (let [ipfs (.create IPFSHttpClient "http://127.0.0.1:5001")
+            orbitdb (<p!
+                     (->
+                      (.createInstance
+                       OrbitDB ipfs
+                       (clj->js
+                        {"directory" (.join path (.homedir os) "Elsbeth" "orbitdb")}))
+                      (.catch (fn [ex]
+                                (println ex)))))]
+        (println (.. orbitdb -identity -id))))))
